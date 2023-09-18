811a8efa63dbd92f3ea62de081158d0ea266536d
WedleySilva <WedleySilva2105@gmail.com>
2023-09-18 09:32:07 -0300

    Puxei imagem do banco a para a tela Login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Dimensions } from 'react-native'; // Importe Image e Dimensions
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
@@ -18,57 +18,59 @@ const LoginScreen = () => {
   };
   const handleCadastro = () => {
     navigation.navigate('Cadastro');
-  }
+  };
 
   return (
-
-      <View style={styles.bodyContainer}>
-
-        <View style={styles.loginContainer}>
-
-          <View style={styles.loginCard}>
-            
-            <Text style={styles.loginTitle}>Entre na sua Conta</Text>
-
-            <View style={styles.inputWrapper}>
-              <TextInput style={styles.inputField} placeholder="Usuário" />
-            </View>
-
-            <View style={styles.inputWrapper}>
-              <TextInput style={styles.inputField} placeholder="Senha" secureTextEntry />
-            </View>
-
-            <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-              <Text style={styles.loginButtonText}>Entrar</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.CadastroButton} onPress={handleCadastro}>
-              <Text style={styles.CadastroButtonText}>Não possui conta? Clique aqui!</Text>
-            </TouchableOpacity>
-
+    <View style={styles.bodyContainer}>
+      <Image
+        source={{ uri: 'http://127.0.0.1:19003/media/images/5dc02292-7520-400e-b45b-dd4ceb9790bb.png' }}
+        style={styles.backgroundImage}
+        resizeMode="contain"
+      />
+      <View style={styles.loginContainer}>
+        <View style={styles.loginCard}>
+          <Text style={styles.loginTitle}>Entre na sua Conta</Text>
+          <View style={styles.inputWrapper}>
+            <TextInput style={styles.inputField} placeholder="Usuário" />
           </View>
-          
+          <View style={styles.inputWrapper}>
+            <TextInput style={styles.inputField} placeholder="Senha" secureTextEntry />
+          </View>
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.loginButtonText}>Entrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.CadastroButton} onPress={handleCadastro}>
+            <Text style={styles.CadastroButtonText}>Não possui conta? Clique aqui!</Text>
+          </TouchableOpacity>
         </View>
-        </View>
+      </View>
+    </View>
   );
 };
 
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
-
-    loginContainer: {
+  bodyContainer: {
+    flex: 1,
+    backgroundColor: '#B8B8B8',
+    alignItems: 'center', // Alinha o conteúdo ao centro verticalmente
+    justifyContent: 'flex-start', // Alinha o conteúdo ao topo da tela
+  },
+  backgroundImage: {
+    width: 300,
+    height: 250,
+    resizeMode: 'cover',
+    marginTop: 50, // Espaço do topo da tela
+  },
+  loginContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  bodyContainer: {
-    justifyContent: 'center',
-    height: '100%',
-    width: '100%',
-    backgroundColor: '#B8B8B8', 
-  },
   loginCard: {
     backgroundColor: '#fff',
-    borderColor: '#00b5b2', // Cor ciano
-    borderWidth: 2, // Largura da borda
+    borderColor: '#00b5b2',
+    borderWidth: 2,
     borderRadius: 10,
     shadowColor: '#D0FFF1',
     shadowOffset: { width: 0, height: 10 },
@@ -82,11 +84,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
-  inputLabel: {
-    fontSize: 14,
-    marginBottom: 5,
-    color: '#666',
-  },
   inputField: {
     width: '100%',
     padding: 12,
@@ -94,8 +91,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#f9f9f9',
     borderRadius: 5,
     marginBottom: 15,
-    borderColor: '#00b5b2', // Cor ciano para a borda
-    borderWidth: 2, // Largura da borda
+    borderColor: '#00b5b2',
+    borderWidth: 2,
   },
   loginButton: {
     backgroundColor: '#00b5b2',
@@ -119,5 +116,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
 
 export default LoginScreen;
