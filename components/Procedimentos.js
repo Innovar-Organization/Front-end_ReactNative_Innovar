d4ef8d0fdf626e708c7494ee4395f0673f8219b7
GhostWisher13 <birdrang13@gmail.com>
2023-10-23 08:57:53 -0300

    Componente Procedimentos Criado

---
+++ components/Procedimentos.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Image, Dimensions, Text, ScrollView } from "react-native";
+
+const HomeScreen = () => {
+ 
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
