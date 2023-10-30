7f4dd7284c9c373064324df5b65f51a77959fae4
Wedleysilva <Wedleysilva2105@gmail.com>
2023-10-30 09:29:43 -0300

    Puxando Pacotes e Procedimentos do backend

--- components/Pacotes.js
+++ components/Pacotes.js
@@ -1,13 +1,12 @@
-import { View, Text, StyleSheet, ScrollView, Image, FlatList } from "react-native";
 import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, FlatList } from 'react-native';
 import axios from 'axios';
-
 
 const Pacotes = () => {
   const [pacotes, setPacotes] = useState([]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:19003/api/pacotes/")
+    axios.get('http://127.0.0.1:19003/api/pacotes/')
       .then(response => {
         setPacotes(response.data);
       })
@@ -17,39 +16,39 @@ const Pacotes = () => {
   }, []);
 
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-   
-   <FlatList
-        data={pacotes}
-        keyExtractor={(item) => item.id.toString()} 
-        renderItem={({ item }) => (
+    <View style={styles.container}>
+      <Text style={styles.title}>Veja nossos Pacotes:</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollViewContent}>
+        {pacotes.map(item => (
           <View key={item.id} style={styles.itemContainer}>
-          <View style={styles.imageTextContainer}>
-            <Image
-              source={{ uri: item.imagem }}
-              style={styles.image}
-              resizeMode="contain"
-            />
-            <Text style={styles.text}>{item.nome}</Text>
+            <View style={styles.imageTextContainer}>
+              <Image
+                source={{ uri: item.imagem }}
+                style={styles.image}
+                resizeMode="contain"
+              />
+              <Text style={styles.text}>{item.nome}</Text>
+            </View>
           </View>
-        </View>
-        )}
-      />
-
-    </ScrollView>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexDirection: 'row',
+  },
   container: {
     flex: 1,
-    backgroundColor: "#00b5b2",
+    backgroundColor: '#00b5b2',
   },
   title: {
-    color: "white",
+    color: '#fff',
     fontSize: 25,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
     marginTop: 20,
     marginBottom: 20,
   },
@@ -57,23 +56,23 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   imageTextContainer: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 10,
-    alignItems: "center",
+    alignItems: 'center',
     padding: 20,
     maxHeight: 300,
     maxWidth: 200,
     height: 300,
   },
   image: {
-    width: "100%",
+    width: '100%',
     aspectRatio: 1,
     borderRadius: 10,
   },
   text: {
-    color: "black",
+    color: 'black',
     padding: 30,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 18,
   },
 });
--- components/Procedimentos.js
+++ components/Procedimentos.js
@@ -1,57 +1,31 @@
-import React from "react";
-import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Image, FlatList } from 'react-native';
+import axios from 'axios';
 
-const HorizontalList = () => {
-  const items = [
-    {
-      id: 1,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Limpeza de Pele",
-    },
-    {
-      id: 2,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Massagem Terapêutica",
-    },
-    {
-      id: 3,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Design de Sombrancelhas",
-    },
-    {
-      id: 4,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Massagem com Pedras Quentes",
-    },
-    {
-      id: 5,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Design de Cílios",
-    },
-    {
-      id: 6,
-      image:
-        "http://localhost:19003/media/images/5d5d9493-3be7-45e9-b8ea-09c8e63860f7.png",
-      text: "Quiropraxia",
-    },
-  ];
+const Procedimentos = () => {
+  const [procedimentos, setProcedimentos] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:19003/api/procedimentos/')
+      .then(response => {
+        setProcedimentos(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar os Procedimentos:', error);
+      });
+  }, []);
 
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {items.map((item) => (
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollViewContent}>
+      {procedimentos.map(item => (
         <View key={item.id} style={styles.itemContainer}>
           <View style={styles.imageTextContainer}>
             <Image
-              source={{ uri: item.image }}
+              source={{ uri: item.imagem }}
               style={styles.image}
               resizeMode="contain"
             />
-            <Text style={styles.text}>{item.text}</Text>
+            <Text style={styles.text}>{item.nome}</Text>
           </View>
         </View>
       ))}
@@ -59,25 +33,19 @@ const HorizontalList = () => {
   );
 };
 
-const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Escolha seu Procedimento:</Text>
-      <HorizontalList />
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexDirection: 'row', // Alinha os itens lado a lado
+  },
   container: {
     flex: 1,
-    backgroundColor: "#00b5b2",
+    backgroundColor: '#00b5b2',
   },
   title: {
-    color: "white",
+    color: 'white',
     fontSize: 25,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
     marginTop: 20,
     marginBottom: 20,
   },
@@ -85,25 +53,25 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   imageTextContainer: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 10,
-    alignItems: "center",
+    alignItems: 'center',
     padding: 20,
     maxHeight: 300,
     maxWidth: 200,
     height: 300,
   },
   image: {
-    width: "100%",
+    width: '100%',
     aspectRatio: 1,
     borderRadius: 10,
   },
   text: {
-    color: "black",
+    color: 'black',
     padding: 30,
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 18,
   },
 });
 
-export default HomeScreen;
+export default Procedimentos;
